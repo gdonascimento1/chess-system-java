fb5a32da7380e3b71fe57e6adc09b6257c4db658
Gabriel Nascimento <gdonascimento1@saddleback.edu>
2020-03-19 11:05:09 -0700

    Position class added

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,14 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("Testing if remore repository is connected with local repository");
+
+	Position pos = new Position (2,5);
+	System.out.println(pos);
+	
 	}
 
 }
